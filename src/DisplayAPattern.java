9a8038bfd6737ef9cad75db5045bb44ad8e21a54
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-10-24 13:48:05 +0200

    Initial commit

---
+++ src/DisplayAPattern.java
@@ -0,0 +1,6 @@
+public class DisplayAPattern {
+    public static void main(String[] args)   {
+        System.out.print(
+                "    J \n J aaa    v     vaaa \n J  J  aa    v v   a a \n J   aaaa    v    aaaa" );
+    }
+}
